6c5ce9c8358608cd747de04883f657cd26b4aee0
Hello World <joaop.codes@gmail.com>
2024-12-02 23:01:41 -0300

    .

--- src/js/script.js
+++ src/js/script.js
@@ -1,68 +1,89 @@
-// Função que cria item no Board com lista predefinida
-document.getElementById("addItemBtnPredef").addEventListener("click", function () {
-    const item = document.createElement('div');
-    item.classList.add('item');
+// Função para formatar o número de telefone
+function formatPhoneNumber(phoneNumber) {
+  // Remove todos os caracteres que não sejam números
+  const cleaned = ("" + phoneNumber).replace(/\D/g, "");
 
-    const qtyInput = document.createElement('input');
-    qtyInput.type = 'number';
-    qtyInput.placeholder = 'Qtd';
+  // Verifica se o número tem o tamanho correto
+  const match = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/);
+
+  // Se o número for válido, retorna no formato (99) 99999-9999
+  if (match) {
+    return `(${match[1]}) ${match[2]}-${match[3]}`;
+  }
+
+  // Retorna o número original se a formatação falhar
+  return phoneNumber;
+}
+
+// Função que cria item no Board com lista predefinida
+document
+  .getElementById("addItemBtnPredef")
+  .addEventListener("click", function () {
+    const item = document.createElement("div");
+    item.classList.add("item");
+
+    const qtyInput = document.createElement("input");
+    qtyInput.type = "number";
+    qtyInput.placeholder = "Qtd";
     qtyInput.required = true;
-    qtyInput.style.width = '25%';
-    qtyInput.step = '0.01';
-    qtyInput.setAttribute('id', 'qty');
+    qtyInput.style.width = "25%";
+    qtyInput.step = "0.01";
+    qtyInput.setAttribute("id", "qty");
 
     // Substituindo o campo de descrição por um select com opções predefinidas
-    const descSelect = document.createElement('select');
+    const descSelect = document.createElement("select");
     descSelect.required = true;
-    descSelect.style.width = '50%';
+    descSelect.style.width = "50%";
 
     // Tipos de Peças
     const options = [
-        { "label": "Aranha" },
-        { "label": "Braço mod. Corsa" },
-        { "label": "Braço mod. Digitador com regulagem" },
-        { "label": "Flange diretor com relax" },
-        { "label": "Flange mult regulagem" },
-        { "label": "Flange secretaria" },
-        { "label": "Madeira assento executivo" },
-        { "label": "Madeira assento presidente" },
-        { "label": "Madeira assento secretaria" },
-        { "label": "Madeira assento diretor" },
-        { "label": "Madeira encosto executivo alta" },
-        { "label": "Madeira encosto executivo baixa" },
-        { "label": "Madeira encosto diretor" },
-        { "label": "Madeira encosto presidente" },
-        { "label": "Madeira encosto secretaria" },
-        { "label": "Pistão" },
-        { "label": "Rodízio em polipropileno" },
-        { "label": "Rodízio em silicone p.u" },
-        { "label": "Troca tecido diretor" },
-        { "label": "Troca tecido executiva" },
-        { "label": "Troca tecido presidente" },
-        { "label": "Troca tecido secretaria" }
+      { label: "Aranha" },
+      { label: "Braço mod. Corsa" },
+      { label: "Braço mod. Digitador com regulagem" },
+      { label: "Flange diretor com relax" },
+      { label: "Flange mult regulagem" },
+      { label: "Flange secretaria" },
+      { label: "Madeira assento executivo" },
+      { label: "Madeira assento presidente" },
+      { label: "Madeira assento secretaria" },
+      { label: "Madeira assento diretor" },
+      { label: "Madeira encosto executivo alta" },
+      { label: "Madeira encosto executivo baixa" },
+      { label: "Madeira encosto diretor" },
+      { label: "Madeira encosto presidente" },
+      { label: "Madeira encosto secretaria" },
+      { label: "Pistão" },
+      { label: "Rodízio em polipropileno" },
+      { label: "Rodízio em silicone p.u" },
+      { label: "Troca tecido diretor" },
+      { label: "Troca tecido executiva" },
+      { label: "Troca tecido presidente" },
+      { label: "Troca tecido secretaria" },
     ];
 
-    options.forEach(opt => {
-        const optionElement = document.createElement('option');
-        optionElement.value = opt.label;
-        optionElement.text = opt.label;
-        descSelect.appendChild(optionElement);
+    options.forEach((opt) => {
+      const optionElement = document.createElement("option");
+      optionElement.value = opt.label;
+      optionElement.text = opt.label;
+      descSelect.appendChild(optionElement);
     });
 
     // Exibir o preço automaticamente baseado na opção selecionada
-    const priceDisplay = document.createElement('input');
-    priceDisplay.type = 'number';
-    priceDisplay.placeholder = 'UNI';
+    const priceDisplay = document.createElement("input");
+    priceDisplay.type = "number";
+    priceDisplay.placeholder = "UNI";
     priceDisplay.required = true;
-    priceDisplay.style.width = '25%';
-    priceDisplay.step = '0.01';
-    priceDisplay.setAttribute('id', 'price');
+    priceDisplay.style.width = "25%";
+    priceDisplay.step = "0.01";
+    priceDisplay.setAttribute("id", "price");
 
-    const removeBtn = document.createElement('button');
-    removeBtn.classList.add('removeBtn');
-    removeBtn.textContent = 'x';
-    removeBtn.addEventListener('click', function () {
-        item.remove();
+
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("removeBtn");
+    removeBtn.textContent = "x";
+    removeBtn.addEventListener("click", function () {
+      item.remove();
+      
     });
 
     item.appendChild(qtyInput);
@@ -70,43 +91,49 @@ document.getElementById("addItemBtnPredef").addEventListener("click", function (
     item.appendChild(priceDisplay);
     item.appendChild(removeBtn);
 
-    document.getElementById('board').appendChild(item);
-});
+    document.getElementById("board").appendChild(item);
+   
+  });
 
 // Função que cria item manualmente (campo vazio para preenchimento)
-document.getElementById("addItemBtnManual").addEventListener("click", function () {
-    const item = document.createElement('div');
-    item.classList.add('item');
+document
+  .getElementById("addItemBtnManual")
+  .addEventListener("click", function () {
+    const item = document.createElement("div");
+    item.classList.add("item");
 
-    const qtyInput = document.createElement('input');
-    qtyInput.type = 'number';
-    qtyInput.placeholder = 'Qtd';
+    const qtyInput = document.createElement("input");
+    qtyInput.type = "number";
+    qtyInput.placeholder = "Qtd";
     qtyInput.required = true;
-    qtyInput.style.width = '25%';
-    qtyInput.step = '0.01';
-    qtyInput.setAttribute('id', 'qty');
+    qtyInput.style.width = "25%";
+    qtyInput.step = "0.01";
+    qtyInput.setAttribute("id", "qty");
 
     // Campo de descrição manual
-    const descInput = document.createElement('input');
-    descInput.type = 'text';
-    descInput.placeholder = 'Descrição do item';
+    const descInput = document.createElement("input");
+    descInput.type = "text";
+    descInput.placeholder = "Descrição do item";
     descInput.required = true;
-    descInput.style.width = '50%';
+    descInput.style.width = "50%";
 
     // Campo de preço manual
-    const priceInput = document.createElement('input');
-    priceInput.type = 'number';
-    priceInput.placeholder = 'UNI';
+    const priceInput = document.createElement("input");
+    priceInput.type = "number";
+    priceInput.placeholder = "UNI";
     priceInput.required = true;
-    priceInput.style.width = '25%';
-    priceInput.step = '0.01';
-    priceInput.setAttribute('id', 'price');
+    priceInput.style.width = "25%";
+    priceInput.step = "0.01";
+    priceInput.setAttribute("id", "price");
 
-    const removeBtn = document.createElement('button');
-    removeBtn.classList.add('removeBtn');
-    removeBtn.textContent = 'x';
-    removeBtn.addEventListener('click', function () {
-        item.remove();
+    
+
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("removeBtn");
+    removeBtn.textContent = "x";
+    removeBtn.addEventListener("click", function () {
+      item.remove();
+    
     });
 
     item.appendChild(qtyInput);
@@ -114,22 +141,26 @@ document.getElementById("addItemBtnManual").addEventListener("click", function (
     item.appendChild(priceInput);
     item.appendChild(removeBtn);
 
-    document.getElementById('board').appendChild(item);
-});
+    document.getElementById("board").appendChild(item);
+ 
+  });
 
 // Função principal do formulário para gerar o PDF
-document.getElementById("orcamentoForm").addEventListener("submit", function (event) {
+document
+  .getElementById("orcamentoForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
     // Pegando os dados do formulário
     const nome = document.getElementById("nome").value;
     const telefone = document.getElementById("telefone").value;
+    const telefoneFormatado = formatPhoneNumber(telefone);
     const data = document.getElementById("data").value;
     const tcond = document.getElementById("tcond").value;
     const fpag = document.getElementById("fpag").value;
 
     // Formatando a data
-    const [ano, mes, dia] = data.split('-');
+    const [ano, mes, dia] = data.split("-");
     const datebr = `${dia}/${mes}/${ano}`;
 
     let valorTotalItens = 0; // Variável para somar o total do orçamento
@@ -137,12 +168,14 @@ document.getElementById("orcamentoForm").addEventListener("submit", function (ev
     const items = [];
     // Pegando os itens do board
     document.querySelectorAll("#board .item").forEach(function (itemDiv) {
-        const qty = parseFloat(itemDiv.querySelector("input[id='qty']").value);
-        const desc = itemDiv.querySelector("select, input[type='text']").value;
-        const price = parseFloat(itemDiv.querySelector("input[id='price']").value);
-        const totalItem = qty * price;
-        items.push({ qty, desc, price, total: totalItem });
-        valorTotalItens += totalItem; // Somando o total de cada item
+      const qty = parseFloat(itemDiv.querySelector("input[id='qty']").value);
+      const desc = itemDiv.querySelector("select, input[type='text']").value;
+      const price = parseFloat(
+        itemDiv.querySelector("input[id='price']").value
+      );
+      const totalItem = qty * price;
+      items.push({ qty, desc, price, total: totalItem });
+      valorTotalItens += totalItem; // Somando o total de cada item
     });
 
     const { jsPDF } = window.jspdf;
@@ -158,81 +191,87 @@ document.getElementById("orcamentoForm").addEventListener("submit", function (ev
 
     // Desenhando em cima do template
     img.onload = function () {
-        doc.addImage(img, 'JPG', 0, 0, doc.internal.pageSize.width, doc.internal.pageSize.height);
+      doc.addImage(
+        img,
+        "JPG",
+        0,
+        0,
+        doc.internal.pageSize.width,
+        doc.internal.pageSize.height
+      );
 
-        doc.setFont("Helvetica");
-        doc.setFontSize(14);
+      doc.setFont("Helvetica");
+      doc.setFontSize(14);
 
-        // Adicionando os dados do cliente
-        doc.text(125, 90, nome);
-        doc.text(125, 100, telefone);
+      // Adicionando os dados do cliente
+      doc.text(125, 90, nome);
+      doc.text(125, 100, telefoneFormatado);
 
-        // Adicionando os termos e condições
-        const larguraMaxima = 70;
-        const linhasTexto = doc.splitTextToSize(tcond, larguraMaxima);
-        let yPos = 240;
-        linhasTexto.forEach(linha => {
-            doc.text(30, yPos, linha);
-            yPos += 7;
-        });
+      // Adicionando os termos e condições
+      const larguraMaxima = 70;
+      const linhasTexto = doc.splitTextToSize(tcond, larguraMaxima);
+      let yPos = 240;
+      linhasTexto.forEach((linha) => {
+        doc.text(30, yPos, linha);
+        yPos += 7;
+      });
 
-        // Adicionando a forma de pagamento
-        const linhasTexto2 = doc.splitTextToSize(fpag, larguraMaxima);
-        let yPos2 = 268;
-        linhasTexto2.forEach(linha => {
-            doc.text(30, yPos2, linha);
-            yPos2 += 7;
-        });
+      // Adicionando a forma de pagamento
+      const linhasTexto2 = doc.splitTextToSize(fpag, larguraMaxima);
+      let yPos2 = 268;
+      linhasTexto2.forEach((linha) => {
+        doc.text(30, yPos2, linha);
+        yPos2 += 7;
+      });
 
-        // Adicionando a data
-        doc.setTextColor(255, 255, 255);
-        doc.text(25, 56, datebr);
+      // Adicionando a data
+      doc.setTextColor(255, 255, 255);
+      doc.text(25, 56, datebr);
 
-        let ypos = 125;
-        let linhasPorPagina = 10; // Número máximo de linhas por página
-        let linhaAtual = 0;
+      let ypos = 125;
+      let linhasPorPagina = 10; // Número máximo de linhas por página
+      let linhaAtual = 0;
 
-        // Adicionando os itens
-        items.forEach((item, index) => {
-            if (linhaAtual === linhasPorPagina) {
-                // Criar uma nova página com o template de continuação
-                doc.addPage();
-                doc.addImage(
-                imgContinuation,
-                "JPG",
-                0,
-                0,
-                doc.internal.pageSize.width,
-                doc.internal.pageSize.height
-                );
-                
-                ypos = 60; // Reinicia a posição Y na nova página
-                linhaAtual = 0; // Reinicia a contagem de linhas
-            }
+      // Adicionando os itens
+      items.forEach((item, index) => {
+        if (linhaAtual === linhasPorPagina) {
+          // Criar uma nova página com o template de continuação
+          doc.addPage();
+          doc.addImage(
+            imgContinuation,
+            "JPG",
+            0,
+            0,
+            doc.internal.pageSize.width,
+            doc.internal.pageSize.height
+          );
 
-            doc.setTextColor(0, 0, 0);
-            doc.text(13, ypos, item.qty.toString());
-            doc.text(35, ypos, item.desc);
-            doc.text(160, ypos, item.price.toFixed(2));
-            doc.text(180, ypos, item.total.toFixed(2));
-            ypos += 10;
-            linhaAtual++;
-        });
-
-
-        //Pegando numero de paginas
-        const paginas = doc.getNumberOfPages();
-        if (paginas > 1) {
-          doc.setTextColor(0, 0, 0);
-          doc.setFontSize(20);
-          doc.text(140, 285, "TOTAL: R$" + valorTotalItens.toString());
-        } else {
-          doc.setTextColor(0, 0, 0);
-          doc.setFontSize(20);
-          doc.text(140, 230, "TOTAL: R$" + valorTotalItens.toString());
+          ypos = 60; // Reinicia a posição Y na nova página
+          linhaAtual = 0; // Reinicia a contagem de linhas
         }
 
-        // Salvando o PDF
-        doc.save(`Orçamento_${nome}.pdf`);
+        doc.setTextColor(0, 0, 0);
+        doc.text(13, ypos, item.qty.toString());
+        doc.text(35, ypos, item.desc);
+        doc.text(160, ypos, item.price.toFixed(2));
+        doc.text(180, ypos, item.total.toFixed(2));
+        ypos += 10;
+        linhaAtual++;
+      });
+
+      //Pegando numero de paginas
+      const paginas = doc.getNumberOfPages();
+      if (paginas > 1) {
+        doc.setTextColor(0, 0, 0);
+        doc.setFontSize(20);
+        doc.text(140, 285, "TOTAL: R$" + valorTotalItens.toString());
+      } else {
+        doc.setTextColor(0, 0, 0);
+        doc.setFontSize(20);
+        doc.text(140, 230, "TOTAL: R$" + valorTotalItens.toString());
+      }
+
+      // Salvando o PDF
+      doc.save(`Orçamento_${nome}.pdf`);
     };
-});
+  });
